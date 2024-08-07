df315b8664854b7fe36e7af67059b6538532aad4
OliaPetrova <skopindom8@gmail.com>
2024-08-07 15:45:20 +0300

    Add files via upload

---
+++ src/Randoms.java
@@ -0,0 +1,33 @@
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+    private final Random random;
+    private final int min;
+    private final int max;
+
+    public Randoms(int min, int max) {
+        if (min > max) {
+            throw new IllegalArgumentException("Минимальное значение не может быть больше максимального");
+        }
+        this.min = min;
+        this.max = max;
+        this.random = new Random();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public Integer next() {
+                return min + random.nextInt(max - min + 1);
+            }
+        };
+    }
+
+}
